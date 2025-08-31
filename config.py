1b52dbd53a261a34770239f3493c8705dd0ec09d
조건빈 <>
2025-09-01 02:57:52 +0900

    feat: 키움 교체 작업

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+
+# =======================================================
+# 키움증권 API 설정
+# =======================================================
+
+# --- 모의 투자 API 설정 (현재 사용 안함) ---
+KIWOOM_APP_KEY_MOCK = "V9mudQMHawpCK_sILj7nmasU9AZI_zr2sgsUFIxbyb4"
+KIWOOM_SECRET_KEY_MOCK = "erjXUeVqBNA5vZLjBjAfy3HkaAOcJEF3G9X37lGUgA0"
+
+# --- 실전 투자 API 설정 (현재 모든 기능에서 사용) ---
+KIWOOM_APP_KEY_REAL = "KonzIWMxMjeSfZNvKurWldIIc8VRCJGGUvzwibfharc"
+KIWOOM_SECRET_KEY_REAL = "4BYKNbayhDNvGJfFPmElnoEateY5V1cxZv3omqQ8-yc"
+
+# DART API 키 (기존 유지)
+DART_API_KEY = "03ac38be54eb9bb095c2304b254c756ebe73c522"
